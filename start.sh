3f8bbaefdf705e3865d81c4ac4e987daa826914b
Her <101386844+runingHer@users.noreply.github.com>
2023-02-01 17:42:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,10 @@
 
 path=$(pwd)
 chmod 777 reboot.sh
-while [ 1 ]; do
-  if [ -n "$1" ]; then
-    ./reboot.sh $1 $path
-    break
-  else
-    ./reboot.sh 100 $path
-  fi
-done
+if [ -n "$1" ]; then
+  ./reboot.sh $1 $path
+  break
+else
+  ./reboot.sh 100 $path
+fi
+
