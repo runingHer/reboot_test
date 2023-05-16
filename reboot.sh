b7060c1a2534d75aa55a64d91591e933f33c6867
Her <101386844+runingHer@users.noreply.github.com>
2023-05-16 11:21:50 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
-set -x
+#set -x
 
-path=$2
+source /etc/profile
+
 add_config=$(cat /etc/rc.local | grep "reboot.sh")
-if [ ! -d ${path}/reboot_test ]; then
-  mkdir ${path}/reboot_test
+# shellcheck disable=SC2154
+if [ ! -d ${run_path}/test_logs ]; then
+  mkdir ${run_path}/test_logs
 fi
-
 if [ ! -n "$add_config" ]; then
-  echo "bash ${path}/reboot.sh" >>/etc/rc.local
+  echo "bash ${run_path}/reboot.sh" >>/etc/rc.local
 fi
-
+#重启次数控制
 if [ -n "$1" ]; then
-  echo $1 >${path}/reboot_test/count.txt
+  echo $1 >${run_path}/test_logs/count.txt
 fi
-
 total_number() {
-  count_num=$(cat ${path}/reboot_test/count.txt)
-  count=$(cat ${path}/reboot_test/date.txt | wc -l)
+  count_num=$(cat ${run_path}/test_logs/count.txt)
+  count=$(cat ${run_path}/test_logs/date.txt | wc -l)
 }
-
+#bit上报功能检验
+bit_state_check() {
+  bit_state=$(cat /root/apache-tomcat-8.5.23/logs/catalina.out | grep "发送")
+  echo -e "$((count + 1)):\t\c" >>${run_path}/test_logs/bit_state.txt
+  if [ ! -n "$bit_state" ]; then
+    echo "error-----bit state abnormal!" >>${run_path}/test_logs/bit_state.txt
+  else
+    echo "success-----bit state normal!" >>${run_path}/test_logs/bit_state.txt
+  fi
+}
+#检测网络端口号
+net_state_check() {
+  net_prot=$(ifconfig | grep "flags=" | awk -F ":" '{print $1}')
+  port_num=$(ifconfig | grep "flags=" | awk -F ":" '{print $1}' | wc -l)
+  echo -e "$((count + 1)):\t\c" >>${run_path}/test_logs/net_port.txt
+  # shellcheck disable=SC2154
+  for i in ${net_port};do
+    # shellcheck disable=SC2028
+    echo "网口${i}已识别！\t\c" >>${run_path}/test_logs/net_port.txt
+  done
+  echo "网口数量为${port_num}！" >>${run_path}/test_logs/net_port.txt
+}
 main() {
-  echo -e "$((count + 1)):\t\c" >>${path}/reboot_test/date.txt
-  date >>${path}/reboot_test/date.txt
-  sleep 80
+  echo -e "$((count + 1)):\t\c" >>${run_path}/test_logs/date.txt
+  date >>${run_path}/test_logs/date.txt
+  sleep 30
+#  net_state_check
+  sleep 50
   reboot
 }
 total_number &>/dev/null
