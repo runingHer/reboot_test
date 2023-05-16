0b9f4bc560864f356acb563f814144937329a342
Her <101386844+runingHer@users.noreply.github.com>
2023-05-16 11:22:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-path=$(pwd)
+path_state=$(cat /etc/profile | grep "run_path")
+if [ ! -n "$path_state" ]; then
+  echo -e "run_path=\c" >>/etc/profile
+  echo $(pwd) >>/etc/profile
+fi
 chmod 777 reboot.sh
 if [ -n "$1" ]; then
-  ./reboot.sh $1 $path
+  ./reboot.sh $1
 else
-  ./reboot.sh 100 $path
+  ./reboot.sh 100
 fi
-
